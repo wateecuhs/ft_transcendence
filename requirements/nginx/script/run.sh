52d3bc38ccfe8eee7e5245642f013206c9890d55
panger <panger@debian.panger.42.fr>
2024-10-22 15:10:02 +0100

    rm unused files

--- requirements/nginx/script/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/L=LYON/O=42/CN=panger.42.fr" -g "daemon off;"
