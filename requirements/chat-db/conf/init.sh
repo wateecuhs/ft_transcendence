098f28b1a8f0810d659c02ae3cc7fedde21697a7
panger <panger@debian.panger.42.fr>
2024-11-11 14:12:35 +0000

    init

--- requirements/chat-db/conf/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER "$DB_USER" WITH PASSWORD '$DB_PASSWORD';
-    CREATE DATABASE "$DB_NAME" WITH OWNER "$DB_USER";
-EOSQL
