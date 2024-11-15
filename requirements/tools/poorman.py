aad446704dfb2999efce0d4582249a5346ed688e
Pierre ANGER <waticouzz@gmail.com>
2024-11-15 14:19:57 +0100

    poorman

--- requirements/tools/poorman.py
+++ requirements/tools/poorman.py
@@ -1,20 +1,31 @@
 import requests
 
-r = requests.post("https://localhost:8443/matchmaking/rooms/", verify=False, data={"label": "room1"})
-print(r.text)
-
-# while True:
-#     cmd = input("Enter a command: ")
-#     match cmd:
-#         case "get":
-#             path = input("Enter the path: ")
-#             r = requests.get(f"https://localhost:8443/{path}/", verify=False)
-#             print(r.json())
-#         case "post":
-#             path = input("Enter the path: ")
-#             r = requests.post(f"https://localhost:8443/{path}/", verify=False)
-#             print(r.json())
-#         case "exit":
-#             break
-#         case _:
-#             print("cmd can only be 'get', 'post', or 'exit'")
+while True:
+    cmd = input("Enter a command: ")
+    match cmd:
+        case "get":
+            path = input("Enter the path: ")
+            data ={}
+            while path != "stop":
+                entry = input("Enter the key-value pair (empty line for eof): ")
+                if entry == "":
+                    break
+                key, value = entry.split()
+                data[key] = value
+            r = requests.get(f"https://localhost:8443/{path}/", verify=False, data=data)
+            print(r.json())
+        case "post":
+            path = input("Enter the path: ")
+            data ={}
+            while path != "stop":
+                entry = input("Enter the key-value pair (empty line for eof): ")
+                if entry == "":
+                    break
+                key, value = entry.split()
+                data[key] = value
+            r = requests.post(f"https://localhost:8443/{path}/", verify=False, data=data)
+            print(r.json())
+        case "exit":
+            break
+        case _:
+            print("cmd can only be 'get', 'post', or 'exit'")
