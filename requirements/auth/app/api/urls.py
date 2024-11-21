9938e8998d266af630cdd3f913f012fca9e88f8c
earaujo42 <earaujo@student.42.fr>
2024-11-21 15:15:59 +0100

    add path in url.py

--- requirements/auth/app/api/urls.py
+++ requirements/auth/app/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import LoginAPI, Register, UserInfo, ConfirmToken, refresh, UserInfoId, UserInfoUsername
+from rest_framework.views import APIView
+from .views import LoginAPI, Register, UserInfo, ConfirmToken, refresh, UserInfoId, UserInfoUsername, MatchHistory, MatchHistoryId, MatchHistoryUsername
 
 urlpatterns = [
     path("register/", Register.as_view(), name="register"),
@@ -8,5 +9,8 @@ urlpatterns = [
     path("user/<str:username>", UserInfoUsername.as_view(), name="User Info Username"),
     path("username/<uuid:id>", UserInfoId.as_view(), name="User Info Id"),
     path("token/", ConfirmToken.as_view(), name="Token"),
-    path("refresh", refresh.as_view(), name="Refresh")
+    path("refresh", refresh.as_view(), name="Refresh"),
+    path("match_history/me", MatchHistory.as_view(), name="Match History"),
+    path("match_history/<str:username>", MatchHistoryUsername.as_view(), name="Match History Username"),
+    path("match_history/<uuid:id", MatchHistoryId.as_view(), name="Match History Id")
 ]
