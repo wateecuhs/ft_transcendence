548b401146cd682ebf8fb2cc669c67b928cfd71b
earaujo42 <earaujo@student.42.fr>
2024-11-22 11:07:11 +0100

    patch url.py

--- requirements/auth/app/api/urls.py
+++ requirements/auth/app/api/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path("login/", LoginAPI.as_view(), name="login"),
     path("user/me/", UserInfo.as_view(), name="User Info"),
     path("user/<str:username>", UserInfoUsername.as_view(), name="User Info Username"),
-    path("username/<uuid:id>", UserInfoId.as_view(), name="User Info Id"),
+    path("user/<uuid:id>", UserInfoId.as_view(), name="User Info Id"),
     path("token/", ConfirmToken.as_view(), name="Token"),
     path("refresh", refresh.as_view(), name="Refresh"),
     path("match_history/me", MatchHistory.as_view(), name="Match History"),
     path("match_history/<str:username>", MatchHistoryUsername.as_view(), name="Match History Username"),
-    path("match_history/<uuid:id", MatchHistoryId.as_view(), name="Match History Id")
+    path("match_history/<uuid:id>", MatchHistoryId.as_view(), name="Match History Id")
 ]
