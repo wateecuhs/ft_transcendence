91f016989e24c3ede4fc83bee1d53740bf3c86cf
Pierre ANGER <waticouzz@gmail.com>
2024-12-10 12:32:11 +0100

    added ai_game

---
+++ requirements/ai_game/app/ai_game/consumers.py
@@ -0,0 +1,81 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+import asyncio
+from .game import Room
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+BALL_RADIUS = 10
+FPS = 60
+
+rooms = {}
+
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print("Connected", flush=True)
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        if self.room_name not in rooms:
+            rooms[self.room_name] = Room(self.room_name)
+        self.room = rooms[self.room_name]
+
+        if not await self.room.add_player(self):
+            await self.close()
+            return
+
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
+
+        if not hasattr(self.room, "game_loop"):
+            self.room.game_loop = asyncio.create_task(self.update_game_state())
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        await self.room.remove_player(self)
+
+        if len(self.room.players) == 0:
+            self.room.game_loop.cancel()
+            del rooms[self.room_name]
+
+    async def receive(self, text_data):
+        command = json.loads(text_data)
+        if command.get('type') == 'disconnect':
+            print("Disconnected", flush=True)
+            # await self.disconnect(1000)
+            self.room.game_loop.cancel()
+            del rooms[self.room_name]
+            return
+
+        player_index = self.room.players.index(self)
+
+        if player_index == 0:
+            if "move_left_up" in command:
+                self.room.keys_pressed["move_left_up"] = command["move_left_up"]
+            if "move_left_down" in command:
+                self.room.keys_pressed["move_left_down"] = command["move_left_down"]
+
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                "type": "game_update",
+                "message": text_data
+            }
+        )
+
+    async def game_update(self, event):
+        await self.send(text_data=event['message'])
+
+    async def update_game_state(self):
+        while True:
+            self.room.move_paddle_ai()
+
+            game_state = await self.room.update_game_state()
+            await self.channel_layer.group_send(
+                self.room_name,
+                {
+                    "type": "game_update",
+                    "message": json.dumps(game_state)
+                }
+            )
+            await asyncio.sleep(1 / FPS)
