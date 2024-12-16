59a6973fdc3c93a312d74cf9b3335df595fa8ea7
Pierre ANGER <waticouzz@gmail.com>
2024-12-16 16:24:35 +0100

    covered possible 500

--- requirements/chat/app/chat/views.py
+++ requirements/chat/app/chat/views.py
@@ -20,7 +20,7 @@ class MessagesView(APIView):
                 'created_at': formatted_time
             }})
         return Response(message_history)
-    
+
 class FriendsView(APIView):
     def get(self, request):
         try:
@@ -34,7 +34,11 @@ class FriendsView(APIView):
                 return Response("User not found", status=404)
             print([str(username) for username in user.get_friends()], flush=True)
             return Response([str(username) for username in user.get_friends()])
+        except ValueError:
+            return Response("Invalid token", status=403)
+        except IndexError:
+            return Response("No token provided", status=401)
         except AttributeError:
             return Response("No token provided", status=401)
         except Exception as e:
-            return Response("An error occurred", status=500)
+            return Response(f"An error occurred {e}", status=500)
