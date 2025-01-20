efbdbb331988170eb3ca216199e946d8144358a0
Pierre ANGER <waticouzz@gmail.com>
2025-01-20 11:15:44 +0100

    added secrets

--- requirements/ai_game/app/core/settings.py
+++ requirements/ai_game/app/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*oc2w(dchi)t%gc3ho&qu61v#3ldt#&h%+nh72y4lq)!yg!nn^'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
